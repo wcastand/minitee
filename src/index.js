4ab2b13ddb8d07dfb1e1acb7a1f10273b8f6bd7e
William Castandet <wcastand@gmail.com>
2016-06-05 18:08:07 +0200

    refactor(): rewrite the library and add some features
    add init command and options on list. Add the possibility to create multiple file in one command
    
    BREAKING CHANGE: on list and how templates works

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
-
 import program from 'commander'
+
+import init from './init'
 import list from './list'
-import cli from './cli'
+import create from './create'
+
+program
+  .version('0.0.1')
+  .description('Mini and really simple template engine.')
+
+program
+  .command('init')
+  .description('Create basic file conf for minitee in the current directory')
+  .option('-s, --src <path>', 'Folder of your templates.')
+  .option('-d, --dest <path>', 'Default folder for the created files.')
+  .action(init)
 
 program
   .command('list')
-  .option('-a, --all', 'Show variables')
+  .description('List all the templates available.')
+  .option('-a, --all', 'Show variables in the template.')
   .action(list)
+
 program
   .arguments('<cmd> [args...]')
-  .action(cli)
+  .description('Create file(s) from a template.')
+  .action(create)
 
 program.parse(process.argv)
